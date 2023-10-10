0c31a69d3d6362d74f17804d540b345815c60e1a
Mafe-lang <mafe0002@edu.mercantec.dk>
2023-10-10 23:44:55 +0200

    Rainbow CC Version 1.2
    + Buttons
    + Functions
    + Script nederst i body

--- app.js
+++ app.js
@@ -1,12 +1,52 @@
 
 
 
+// Variabler
+   let count = 0;
+   const button = document.getElementById('cookieKnap');
+   const countDisplay = document.getElementById('cookiesDisplay');
+
+
+   let countSec = 0;
+   
 
 
 
 
 
 
+
+
+// For- og while-loops
+
+
+
+
+
+
+
+
+
+
+//EventListeners
+
+button.addEventListener('click', handleClick);
+
+
+
+
+
+
+
+// Funktioner
+
+function handleClick() {
+    
+    count++;
+
+    
+    countDisplay.textContent = count + " Cookies";
+}
 
 
 
